cf88a6160d10d80d650ec2bdbf784903029bdb0a
Vitaly <vitaly.korsunov@gmail.com>
2023-12-09 14:44:20 +0200

    Add project files.

---
+++ Dal/Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dal.Data;
+using Dal.Repository.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Product.Update(obj);
+        }
+
+
+    }
+}
